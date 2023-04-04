2c0e393d153c31cefd4576b5353f8b1b003509b3
Sahilpatil3205 <129884874+Sahilpatil3205@users.noreply.github.com>
2023-04-04 23:38:18 +0530

    Add files via upload

---
+++ numberssum.py
@@ -0,0 +1,9 @@
+
+a=int(input("Enter the numbers:-"))
+b=int(input("Enter the numbers:-"))
+c=int(input("Enter the numbers:-"))
+sqrt_a=a**2
+sqrt_b=b**2
+sqrt_c=c**2
+sum=sqrt_a+sqrt_b+sqrt_c
+print("The sum of squares of numbers :-",sum)
